25303ceb5508e083eb3d99f67ea048eed34aee91
Sambi <sambisingh289@gmail.com>
2025-05-21 11:55:39 -0500

    Proper dog image shows up now

--- app.py
+++ app.py
@@ -18,22 +18,24 @@ headers = {
 response = requests.get("https://api.thedogapi.com/v1/images/search", headers=headers)
 print(response.status_code)
 print(response.json())
-print(f"This dogs name is {response.json()[0]["breeds"][0]["name"]}")
+if len(response.json()[0]["breeds"][0]["name"]) > 0:
+    print(f"This doggies breed is a {response.json()[0]["breeds"][0]["name"]}")
+else:
+    print("Sorry! couldn't find the breed of this dog :(")
+    
+urlImage = response.json()[0]["url"]
+print(urlImage)
+#print(f"This dogs name is {response.json()[0]["breeds"][0]["name"]}")
 #print(response.json()["breeds"][0]["name"])
-breed_list = requests.get("https://dog.ceo/api/breeds/list/all")
-if(breed_list.status_code == 200):
-    print("WOrking!")
-endpoint = requests.get("https://dog.ceo/api/breeds/image/random")
-if endpoint.json()["status"] == "success":
-    urlImage = endpoint.json()["message"]
+
     #img = Image.open(urlImage)
     #Get the image content, in bytes
     #stream, handles when data from the response object is read to the file, by default its
     # not done immeidatley, but if True then will automatically download content to file,
     # very useful for streaming, processing huge images, checking header content, etc.
     
-    img_content = requests.get(urlImage, stream=True) #Hold response OBEJCT! Headers, Status code, etc!
-    img = Image.open(io.BytesIO(img_content.content)) #.content gets us a sequence of bytes
-    img.show()
-    print(urlImage)
+img_content = requests.get(urlImage, stream=True) #Hold response OBEJCT! Headers, Status code, etc!
+img = Image.open(io.BytesIO(img_content.content)) #.content gets us a sequence of bytes
+img.show()
+#print(urlImage)
     #print(f"Dog breed name: {}")
